2ccf41acddd5cbbe2332357496052ba5c41ae322
Yevhenii Hyzyla <hyzyla@MacBook-Pro-Olena.local>
2020-06-06 16:53:35 +0300

    Add few models

--- app/models.py
+++ app/models.py
@@ -3,31 +3,29 @@ from flask_security import RoleMixin, UserMixin
 from . import db
 
 
-class UserRoles(db.Model):
-    __tablename__ = 'user_roles'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
-
-
-class Role(db.Model, RoleMixin):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.Text, unique=True)
-    description = db.Column(db.Text)
-
-    def __repr__(self):
-        return self.name
-
-
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.Text, unique=True)
-    password = db.Column(db.Text)
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary='user_roles')
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.UUID(), primary_key=True)
+    text = db.Column(db.Text)
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    
+    id = db.Column(db.UUID(), primary_key=True)
+    parent_id = db.Column(db.UUID(), db.ForeignKey(''))
+    post_id = db.Column(db.UUID(), db.ForeignKey('comments.id'), nullable=True)
+    
+
+
+class Comments
