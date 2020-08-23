72b2cc46870ed57df06c462bc3d3e0a17edfbb07
Yevhenii Hyzyla <hyzyla@MacBook-Pro-Olena.local>
2020-08-23 15:26:39 +0300

    Add title for page

--- web/components/PostPage/PostPage.tsx
+++ web/components/PostPage/PostPage.tsx
@@ -3,6 +3,7 @@ import css from './PostPage.module.css';
 import { PostCard } from "../PostCard/PostCard";
 import {Post, Comment, User} from "../../types";
 import {Comments} from "../CommentsList/Comments";
+import Head from "next/head";
 
 export const postContext = React.createContext<Post | undefined>(undefined);
 export const currentUserContext = React.createContext<User | undefined>(undefined);
@@ -13,11 +14,23 @@ interface Props {
     currentUser?: User,
 }
 
+const buildTitle = (post: Post) => {
+    let titleChunks = post.text.split(' ').slice(0, 6);
+    let title = titleChunks.join(' ');
+    if (titleChunks.length == 6) {
+        title = `${title}...`;
+    }
+    return `${title} | TComments`
+}
+
 export const PostPage: FC<Props> = (props) => {
     const { Provider: PostContextProvider } = postContext;
     const { Provider: CurrentUserContextProvider } = currentUserContext;
     return (
         <div className={css.root}>
+            <Head>
+              <title>{buildTitle(props.post)}</title>
+            </Head>
             <div className={css.article}>
                 <PostCard post={props.post}/>
                 <PostContextProvider value={props.post}>
